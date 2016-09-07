4d2b365f33f5316674fb64e9d5f2238575a8a639
hussien89aa <hussien89aa@yahoo.com>
2016-09-07 17:51:28 -0400

    'Heartbeattactic'

---
+++ Heartbeat tactic/HeartBeatTactic/CarApp/HeartBeatSender.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CarApp
+{
+    class HeartBeatSender
+    {
+       TcpListener MYTcpListener;
+        Socket MySocket;
+        string TextInput = "";
+         Boolean IsWorking = true;
+        static String ServerIPheartbeat = "127.0.0.1";
+        public HeartBeatSender()
+        {
+           
+          
+                Thread myth;
+                //start the  heartbeat sender
+                myth = new Thread(new System.Threading.ThreadStart(REcivedData));
+                myth.Start();
+
+                Console.ReadKey();
+            
+        }
+
+          public void REcivedData()
+        {
+            try
+            {
+
+                Console.WriteLine("Car heartbeat is working");
+                ManageLocation manageLocation = new ManageLocation();
+
+                while (IsWorking)
+                {
+                   
+                    String LastLocation = "I am a live";
+                    manageLocation.send_Data(ServerIPheartbeat, 9004, LastLocation);
+                    Thread.Sleep(2000);
+                }
+                Console.WriteLine("Car heartbeat is Stopped");
+            }
+            catch (Exception ex2)
+            {
+                //MessageBox.Show(ex2.Message);
+            }
+        }
+
+        public void send_Data(string ip_adderss, int port_number, string the_message)
+        {
+            try
+            {
+                TcpClient myclient981 = new TcpClient(ip_adderss, port_number);
+                NetworkStream myns981 = myclient981.GetStream();
+                StreamWriter mysw981 = new StreamWriter(myns981);
+                mysw981.Write(the_message);
+                mysw981.Close();
+                myns981.Close();
+                myclient981.Close();
+
+
+            }
+            catch (Exception ex)
+            {// miss connection to the server switch to backup server
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Heartbeat tactic/HeartBeatTactic/HeartBeatTactic/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HeartBeatTactic
+{
+    class Program
+    {
+        static Boolean IsWorking = true;
+        static TcpListener MYTcpListener;
+        static Socket MySocket;
+        static string TextInput = "";
+        static DateTime LastUpdateTime;
+        static int CheckingInterval = 3000;//we chekc every 3 sec
+        static int ExpireTime = 5; // expire after 5 sec
+        static void Main(string[] args)
+        {
+            LastUpdateTime = DateTime.Now;
+            string startupPath = Environment.CurrentDirectory;
+           // Console.WriteLine(startupPath);
+           Console.WriteLine("This App will be harbeat that monitor  car app, \n it user port 9004");
+            Thread myth;
+            //start server
+            myth = new Thread(new System.Threading.ThreadStart(HeartBeatReciver));
+            myth.Start();
+
+            Thread myth1 = new Thread(new System.Threading.ThreadStart(CheckAlive));
+            myth1.Start();
+
+            Console.ReadKey();
+        }
+
+        //update last time we recive car location  
+        static void UpdateTheTime()
+        {
+            LastUpdateTime = DateTime.Now;
+        }
+        // check if the car is alive
+        static void CheckAlive()
+        {
+            while (IsWorking)
+            {
+                Thread.Sleep(CheckingInterval);
+                // rerun the user app
+                TimeSpan span = DateTime.Now.Subtract(LastUpdateTime);
+                if (span.TotalSeconds> ExpireTime)
+                {
+                    Console.WriteLine("Re-Run the App again");
+                    //  Process.Start("C:\\Users\\ASUS S550C\\Desktop\\SoftwareArticlureExample\\HeartBeatTactic\\CarApp\\bin\\Debug\\CarApp.exe");
+                    Process.Start("CarApp.exe");
+
+
+                }
+
+
+
+            }
+        }
+        //reading car location
+        static public void HeartBeatReciver()
+        {
+            try
+            {
+                MYTcpListener = new TcpListener(IPAddress.Any, 9004);
+                MYTcpListener.Start();
+                Console.WriteLine("heartbeat is working");
+                while (IsWorking)
+                {
+                    try
+                    {
+                        MySocket = MYTcpListener.AcceptSocket();
+                        NetworkStream MyNetworkStream = new NetworkStream(MySocket);
+                        StreamReader mysr = new StreamReader(MyNetworkStream);
+                        TextInput = mysr.ReadToEnd();
+                        Console.WriteLine(TextInput);
+                        UpdateTheTime();
+
+                    }
+                    catch (Exception ex)
+                    {
+                        //  MessageBox.Show(ex.Message);
+                    }
+                }
+                Console.WriteLine("heartbeat is Stopped");
+            }
+            catch (Exception ex2)
+            {
+                //MessageBox.Show(ex2.Message);
+            }
+        }
+    }
+}
